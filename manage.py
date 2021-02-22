27b39f6c61d7a2ee4f58dbd3e3e153fb0b1d6bb3
olivares-andres <andresolivares1986@gmail.com>
2021-02-22 13:17:27 -0300

    se agregan base de datos, enviar correo y validación JWT token

--- manage.py
+++ manage.py
@@ -4,44 +4,161 @@ from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from flask_cors import CORS
 from flask_bcrypt import Bcrypt
-from flask_mail import Mail
-from models import db
+from flask_mail import Mail, Message
+from models import db, User
 from flask_jwt_extended import create_access_token
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
 from flask_jwt_extended import JWTManager
+from werkzeug.utils import secure_filename
 
 
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+UPLOAD_FOLDER = os.path.join(BASE_DIR, 'static')
+ALLOWED_IMAGES_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
 app.config['DEBUG'] = 'True'
 app.config['ENV'] = 'development'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(BASE_DIR, 'codekids.sqlite3')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = 'super-secret'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = '465'
 app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_DEBUG'] = True
 app.config['MAIL_USERNAME'] = '4geeks.server@gmail.com'
-app.config['MAIL_PASSWORD'] = '4geeks12345'
+app.config['MAIL_PASSWORD'] = 'mpiiqyjxxfvwvofx'
 JWTManager(app)
 db.init_app(app)
 mail = Mail(app)
 bcrypt = Bcrypt(app)
-Migrate(app)
+Migrate(app, db)
 CORS(app)
 
 manager = Manager(app)
 manager.add_command("db", MigrateCommand)
 
+#funcion de extensiones permitidas
+def allowed_images_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_IMAGES_EXTENSIONS
+
+#funcion de enviar un mensaje
+def send_mail(subject, sender, recipients, message):
+    #estos son los campos del email
+    msg = Message(subject,
+                  sender=sender,
+                  recipients=[recipients])
+    #el contenido del mensaje a enviar
+    msg.html = message
+    #se envia finalmente el e-mail
+    mail.send(msg)
 
 @app.route('/')
 def root():
     return render_template('index.html')
 
+@app.route('/api/register', methods=['POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username', None)
+        password = request.form.get('password', None)
+        
+        if not username or username == '':
+            return jsonify({"msg": "campo de username es obligatorio"})
+
+        if not password or password == '':
+            return jsonify({"msg": "campo de password es obligatorio"})
+        
+        #Vamos a validar que el usuario no exista previamente en la base de datos
+        user = User.query.filter_by(username = username).first()
+        
+        #si existe un usuario entonces devolvemos mensaje de error
+        if user:
+            return jsonify({"msg": "Username existe"}), 400
+
+        if 'avatar' in request.files:
+            #AQUI CAPTURO EL ARCHIVO DE IMAGEN
+            avatar = request.files['avatar']
+            if avatar.filename != '':           
+                if avatar and allowed_images_file(avatar.filename):
+                    filename = secure_filename(avatar.filename)
+                    avatar.save(os.path.join(os.path.join(app.config['UPLOAD_FOLDER'], 'img/avatars'), filename))
+                else:
+                    return jsonify({"msg": "tipo de imagen no permitida o extension incorrecta"})
+        
+        user = User()
+        user.username = username
+        #ahora vamos a usar Bcrypt para encriptar el password y no sea visible
+        user.password = bcrypt.generate_password_hash(password)        
+
+        #validamos si el archivo de imagen avatar tiene algún nombre en caso contrario vamos a validar el nombre por defecto
+        if 'avatar' in request.files:
+            user.avatar = filename
+
+        #aqui hacemos el insert a la db
+        db.session.add(user)
+        #aqui hacemos el commit para validar
+        db.session.commit()
+
+        #aqui le indicamos que le enviaremos al usuario al registrarse
+        html = render_template('emails/email-register.html', user=user)
+
+        send_mail("Registro", "4geeks.server@gmail.com", user.username, html)
+
+        #generamos el token de accesso al usuario
+        access_token = create_access_token(identity=user.username)
+        #creamos un diccionario con los datos de acceso y los datos del usuario
+        data = {
+            "access_token": access_token,
+            "user": user.serialize()
+        }
+        #retornamos todos los datos
+        return jsonify(data), 200
+
+@app.route('/api/login', methods=['POST'])
+def login():
+    if request.method == 'POST':
+        #ahora validamos por un JSON
+        username = request.json.get('username', None)
+        password = request.json.get('password', None)
+        
+        if not username or username == '':
+            return jsonify({"msg": "campo de username es obligatorio"})
+
+        if not password or password == '':
+            return jsonify({"msg": "campo de password es obligatorio"})
+        
+        #Vamos a validar que el usuario no exista previamente en la base de datos
+        user = User.query.filter_by(username = username).first()
+        
+        #si NO existe un usuario entonces devolvemos mensaje de error
+        if  not user:
+            return jsonify({"msg": "Username existe"}), 400
+        
+        #Validamos la contraseña a traves del hash encriptado si lo que ingresamos es igual entonces pasa 
+        if bcrypt.check_password_hash(user.password, password):
+            #generamos el token de accesso al usuario
+            access_token = create_access_token(identity=user.username)
+            #creamos un diccionario con los datos de acceso y los datos del usuario
+            data = {
+                "access_token": access_token,
+                "user": user.serialize()
+            }
+            #retornamos todos los datos
+            return jsonify(data), 200
+        else:
+            return jsonify({"msg": "nombre de usuario o contraseña incorrecta, verifique sus datos"}), 401
+
+
+
+#para crear la base de datos usar los siguientes comandos:
+# python manage.py db init
+# python manage.py db migrate
+# python manage.py db upgrade
 
 if __name__ == '__main__':
     manager.run()
--- models.py
+++ models.py
@@ -1,3 +1,19 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique =True, nullable = False)
+    password = db.Column(db.String(100), nullable= False)
+    avatar = db.Column(db.String(100), nullable=True, default = 'default.png')
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "avatar": self.avatar
+        }
+
+    
